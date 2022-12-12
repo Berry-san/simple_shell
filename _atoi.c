73fb1ad47e040854d0a6636561c7c4a393ed0ea5
Mbah Nkemdinma <nkem_mbah@hotmail.com>
2022-12-12 03:53:05 -0800

    Updated

--- _atoi.c
+++ _atoi.c
@@ -55,14 +55,14 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			sign *= -1;
 
-			if (s[i] >= '0' && s[i] <= '9')
-			{
-				flag = 1;
-				result *= 10;
-				result += (s[i] - '0');
-			}
-			else if (flag == 1)
-				flag = 2;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
 	}
 
 	if (sign == -1)
